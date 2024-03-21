19ec1f549c6abc0fd7d9e67ba066402ca31d8b6f
Melih aus Beeck <melihesad@hotmail.de>
2024-03-21 22:50:20 +0100

    as<y

---
+++ Core/FileName.cs
@@ -0,0 +1,24 @@
+﻿namespace BeeckerWebLurker.Logic.HttpServer;
+
+public class HttpSeverLogic
+{
+    public string BearerToken { get; set; }
+
+    private Task _app;
+
+    public async Task StartServer(string[] args)
+    {
+        args ??= [];
+
+        var builder = WebApplication.CreateSlimBuilder(args);
+
+        var app = builder.Build();
+
+        app.MapPost("/", async httpContext =>
+        {
+            BearerToken = httpContext.Request.Headers.Authorization.FirstOrDefault(h => h?.StartsWith("Bearer") ?? false);
+        });
+
+        _app = app.RunAsync();
+    }
+}
