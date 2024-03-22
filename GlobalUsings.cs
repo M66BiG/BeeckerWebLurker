9bfbab000241bcf88b642a3b61946687fbd5ee3b
Melih KVNO <melihesad@hotmail.de>
2024-03-22 11:37:40 +0100

    SYNC

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -20,4 +20,7 @@ global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Logging;
 
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Hosting;
+
 global using Serilog;
